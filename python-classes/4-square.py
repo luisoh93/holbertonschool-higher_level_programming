409b1a34386fea6f4f8061056b1c6e9f6819216f
luisoh93 <luis_oh@hotmail.com>
2023-06-14 05:57:03 -0700

    update

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -2,10 +2,13 @@
 """
 Square class definition
 """
+
+
 class Square:
     """
     Square class with private instance attribute size
     """
+
     def __init__(self, size=0):
         """
         Args:
