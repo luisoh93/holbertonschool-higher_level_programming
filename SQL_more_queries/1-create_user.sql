7bc12be7d957159bfc3289937d58e848ff139df3
luisoh93 <luis_oh@hotmail.com>
2023-07-27 22:41:18 -0700

    grant user all privileges and create

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,7 @@
--- Creates the user user_0d_1
-GRANT ALL ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- Creates user
+-- create user
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- grant privs
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+-- flush
+FLUSH PRIVILEGES;
