1d84ee73e1ab23b3ae6e66b0aa5301bb671755e1
luisoh93 <luis_oh@hotmail.com>
2023-08-04 21:57:36 -0700

    add 10-model_state_my_get.py

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,29 +1,23 @@
 #!/usr/bin/python3
-# prints the State object with name input
+"""Return State object matching arg"""
 
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    from model_state import Base, State
-    from sys import argv
-    from sqlalchemy.engine.url import URL
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-
-    db = {'drivername': 'mysql+mysqldb',
-            'host': 'localhost',
-            'port': '3306',
-            'username': argv[1],
-            'password': argv[2],
-            'database': argv[3]}
-
-    url = URL(**db)
-    engine = create_engine(url, pool_pre_ping=True)
+    engine = create_engine(
+                'mysql+mysqldb://{}:{}@localhost:3306/{}'
+                .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    session = Session(engine)
 
-    sobj = (argv[4], )
-    try:
-        state = session.query(State).filter(State.name == sobj).one_or_none()
-        print("{}".format(state.id))
-    except:
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).\
+        filter(State.name == argv[4]).order_by(State.id).all()
+    if states:
+        print("{}".format(states[0].id))
+    else:
         print("Not found")
+    session.close()
--- python-object_relational_mapping/A10-model_state_my_get.py
+++ python-object_relational_mapping/A10-model_state_my_get.py
@@ -1,32 +1,29 @@
 #!/usr/bin/python3
-# lists all State objects that contain the letter a from a database
+# prints the State object with name input
+
 
 if __name__ == "__main__":
     from model_state import Base, State
     from sys import argv
-    import sqlalchemy
     from sqlalchemy.engine.url import URL
     from sqlalchemy import create_engine
     from sqlalchemy.orm import Session
 
-    mysql = {'drivername': 'mysql+mysqldb',
+    db = {'drivername': 'mysql+mysqldb',
             'host': 'localhost',
             'port': '3306',
             'username': argv[1],
             'password': argv[2],
-            'database': argv[3],
-            }
+            'database': argv[3]}
 
-    url = URL(**mysql)
-
+    url = URL(**db)
     engine = create_engine(url, pool_pre_ping=True)
     Base.metadata.create_all(engine)
-
     session = Session(engine)
 
-    query = session.query(State).filter(State.name.like('%a%'))\
-            .order_by(State.id)
-    for r in query.all():
-        print("{}: {}".format(r.id, r.name))
-
-    session.close()
+    sobj = (argv[4], )
+    try:
+        state = session.query(State).filter(State.name == sobj).one_or_none()
+        print("{}".format(state.id))
+    except:
+        print("Not found")
