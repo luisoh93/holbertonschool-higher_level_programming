1f10c9a8e570a755ad41c2fba199dd736f465612
luisoh93 <luis_oh@hotmail.com>
2023-08-04 21:32:51 -0700

    add 5-filter_cities.py

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,34 +1,20 @@
 #!/usr/bin/python3
-'''script to list all cities with user inputted state'''
+"""Lists all cities by state passed by user"""
+
+import MySQLdb
+from sys import argv
 
 if __name__ == "__main__":
-    from sys import argv
-    import MySQLdb
-
-    # connect
-    db = MySQLdb.connect(host='localhost',
-            port=3306,
-            user=argv[1],
-            passwd=argv[2],
-            db=argv[3]
-            )
-    # cursor
-    c = db.cursor()
-    # execute
-    state = (argv[4], )
-    cmd = "SELECT cities.name FROM cities JOIN states ON\
-    cities.state_id = states.id AND states.name = %s ORDER BY cities.id ASC"
-
-    c.execute(cmd, state)
-
-    # fetch
-    rows = c.fetchall()
-
-    # print
-    ll = [x[0] for x in rows]
-    together = ", ".join(ll)
-    print(together)
-
-    # close
-    c.close()
-    db.close()
+    conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
+            user=argv[1], passwd=argv[2], db=argv[3])
+    cur = conn.cursor()
+    cur.execute("""
+        SELECT cities.id, cities.name, states.name FROM cities
+        LEFT JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC;
+        """, (argv[4],))
+    query_rows = cur.fetchall()
+    print(", ".join([row[1] for row in query_rows]))
+    cur.close()
+    conn.close()
